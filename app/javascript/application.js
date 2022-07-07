ec0b1cdf4f81f6efc9bb045ba71f6154cd113b72
Frederico Joaquim <frederico22joaquim@gmail.com>
2022-07-07 12:41:26 +0100

    pimeiro commit do template configurado em rails

---
+++ app/javascript/application.js
@@ -0,0 +1,39 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+import "vendor/jquery-1.12.4.min.js"
+import "bootstrap.min.js"
+import "wow.min.js"
+import "jquery-price-slider.js"
+
+import "jquery.meanmenu.js"
+
+
+import "owl.carousel.min.js"
+import "jquery.sticky.js"
+import "jquery.counterup.min.js"
+import "waypoints.min.js"
+import "counterup/counterup-active.js"
+
+import "scrollbar/jquery.mCustomScrollbar.concat.min.js"
+import "scrollbar/mCustomScrollbar-active.js"
+import "metisMenu/metisMenu.min.js"
+import "metisMenu/metisMenu-active.js"
+
+
+import "morrisjs/raphael-min.js"
+import "js/morrisjs/morris.js"
+import "morrisjs/morris-active.js"
+
+import "sparkline/jquery.sparkline.min.js"
+import "sparkline/jquery.charts-sparkline.js"
+import "sparkline/sparkline-active.js"
+import "calendar/moment.min.js"
+import "calendar/fullcalendar.min.js"
+import "calendar/fullcalendar-active.js"
+import "plugins.js"
+import "main.js"
+import "tawk-chat.js"
+
+import "@hotwired/turbo-rails"
+import "controllers"
